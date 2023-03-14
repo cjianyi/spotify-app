5bb75561c184a63bdb460f8ada7198745cc845bf
cjianyi <williamchen33333@gmail.com>
2023-03-14 15:47:59 -0700

    Added oauth2

--- app.py
+++ app.py
@@ -1,21 +1,40 @@
-from flask import Flask, render_template, request
-from templates.main import get_token, search_for_artist, get_songs_by_artist, cleaner
+from flask import Flask, render_template, request, redirect, session
+from templates.main import get_token, search_for_artist, get_songs_by_artist, cleaner, create_spotify_oauth
 from requests import get, post
+from spotipy.oauth2 import SpotifyOAuth 
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    sp_oauth = create_spotify_oauth()
+    auth_url = sp_oauth.get_authorize_url()
+    return redirect(auth_url)
+@app.route('/main', methods=['GET', 'POST'])
+def main():
+    return render_template("home.html")
+
 @app.route('/search', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+
         artist_name = request.form['artist_name']
         token = get_token()
         artist = search_for_artist(token, artist_name)
         
         return render_template('index.html', artist=artist, cleaner=cleaner, get_songs_by_artist=get_songs_by_artist,token=token )
     else:
-        return render_template('index.html')
+        string = "Artist not found"
+        return render_template('index.html', string=string)
+
+@app.route('/redirect')
+def redirectPage():
+    # sp_oauth = create_spotify_oauth()
+    # session.clear()
+    # code = request.args.get("code")
+    # token_info = sp_oauth.get_access_token(code)
+    # session[TOKEN_INFO] = token_info
+    return redirect("/main")
     
 if __name__ == "__main__":
     app.run(debug=True)
--- templates/main.py
+++ templates/main.py
@@ -3,11 +3,41 @@ import os
 import base64
 from requests import post, get
 import json
-
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth 
+from flask import Flask, url_for, session
+import time
 load_dotenv()
 
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
+
+def create_spotify_oauth():
+    return SpotifyOAuth(client_id=client_id,
+                        client_secret=client_secret,
+                        redirect_uri=url_for('redirectPage', _external=True),
+            scope="user-library-read")
+
+def get_token2():
+    token_valid = False
+    token_info = session.get("token_info", {})
+
+    # Checking if the session already has a token stored
+    if not (session.get('token_info', False)):
+        token_valid = False
+        return token_info, token_valid
+
+    # Checking if token has expired
+    now = int(time.time())
+    is_token_expired = session.get('token_info').get('expires_at') - now < 60
+
+    # Refreshing token if it has expired
+    if (is_token_expired):
+        sp_oauth = create_spotify_oauth()
+        token_info = sp_oauth.refresh_access_token(session.get('token_info').get('refresh_token'))
+
+    token_valid = True
+    return token_info, token_valid
 
 def get_token():
     auth_string = client_id + ":" + client_secret
@@ -55,7 +85,17 @@ def cleaner(json):
     for idx, song in enumerate(json):
         result_str += f" {idx + 1}. {song['name']}"
     return result_str
-# token = get_token()
+
+def get_user_data(token):
+    url = "https://api.spotify.com/v1/me/top/artists"
+    headers = get_auth_header(token)
+    result = get(url, headers=headers)
+    print(result.content)
+    # json_result = json.loads(result.content)
+    # print(json_result)
+
+    
+
 # result = search_for_artist(token,"bts")
 # artist_id = result["id"]
 # print(artist_id)
